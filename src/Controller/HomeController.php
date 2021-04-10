e2f0f046e40996bc7d9d8c4d0e7cb9eacf3f06de
eddyRAJA <eddy-rajaonarivelo_student2021@wilder.school>
2021-04-10 20:22:45 +0200

    create showProduct

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: aurelwcs
+ * Date: 08/04/19
+ * Time: 18:40
+ */
+
+namespace App\Controller;
+
+class HomeController extends AbstractController
+{
+    /**
+     * Display home page
+     *
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function index()
+    {
+        return $this->twig->render('Home/index.html.twig');
+    }
+
+    public function showProducts() 
+    {
+        $products = ['guitare', 'bass', 'bonjo', 'cithare', 'lyre'];
+
+        return $this->twig->render('Home/products.html.twig', ['products' => $products]);
+    }
+}
